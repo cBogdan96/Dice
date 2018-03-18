a10d85fa9540f04864bd9991fb2fc10fd47b28b0
cBogdan96 <bogdan_covrig236@yahoo.com>
2018-03-19 01:24:28 +0200

    dice project

---
+++ dice/app/src/main/java/com/courses/bogdan/dice/MainActivity.java
@@ -0,0 +1,13 @@
+package com.courses.bogdan.dice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
